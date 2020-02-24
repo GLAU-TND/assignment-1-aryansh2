498cd528262e4f3df9c784c30298e0c6f7cb4012
aryansh2 <aryan.sharma_iot18@gla.ac.in>
2020-02-24 21:15:15 +0530

    adding getter and setter methods

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -6,4 +6,35 @@ public class Person {
     private String[] contactNo;
     private String email;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String[] getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String[] contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
