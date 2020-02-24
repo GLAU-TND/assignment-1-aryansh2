9bc89d197bac6fd5c88d670f68299b7d7ea0bec7
aryansh2 <aryan.sharma_iot18@gla.ac.in>
2020-02-24 20:45:41 +0530

    creating Node class contructor

--- src/definition/ContactsADT.java
+++ src/definition/ContactsADT.java
@@ -3,6 +3,11 @@ package definition;
 class Node<Person> {
     private Person data;
     private Node<Person> next;
+
+    private Node(Person data, Node<Person> next) {
+        this.data = data;
+        this.next = next;
+    }
 }
 
 public class ContactsADT {
