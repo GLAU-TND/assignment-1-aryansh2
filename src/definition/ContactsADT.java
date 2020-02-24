3991151cc08d7b1b01e1da0f469642ef60c0c36e
aryansh2 <aryan.sharma_iot18@gla.ac.in>
2020-02-24 20:46:39 +0530

    creating getter and setter

--- src/definition/ContactsADT.java
+++ src/definition/ContactsADT.java
@@ -8,6 +8,22 @@ class Node<Person> {
         this.data = data;
         this.next = next;
     }
+
+    public Person getData() {
+        return data;
+    }
+
+    public void setData(Person data) {
+        this.data = data;
+    }
+
+    public Node<Person> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<Person> next) {
+        this.next = next;
+    }
 }
 
 public class ContactsADT {
