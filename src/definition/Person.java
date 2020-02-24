f17df16878d14da1735611c47367917f5ca3c2ce
aryansh2 <aryan.sharma_iot18@gla.ac.in>
2020-02-24 21:17:49 +0530

    overriding toString method to print the following details

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -44,4 +44,19 @@ public class Person {
         this.contactNo = contactNo;
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("-------- * -------- * -------- * --------\n");
+        sb.append("First Name: ").append(firstName).append('\n');
+        sb.append("Last Name: ").append(lastName).append('\n');
+        sb.append("Contact No: ");
+        for (int i = 0; i < contactNo.length; i++) {
+            sb.append((contactNo[0] != null && i == 0 ? contactNo[0] : "") + (contactNo[1] != null && i == 1 ? ", " + contactNo[1] : "") +
+                    (contactNo[2] != null && i == 2 ? ", " + contactNo[i] : ""));
+        }
+        sb.append("\nEmail: ").append(email).append('\n');
+        sb.append("-------- * -------- * -------- * --------\n");
+        return sb.toString();
+    }
 }
