124fc163a0919b20438e8698281f387a88d73b10
aryansh2 <aryan.sharma_iot18@gla.ac.in>
2020-02-24 21:25:41 +0530

    calling up the add method to add the contacts

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import definition.ContactsADT;
+import definition.Person;
 
 import java.util.Scanner;
 
@@ -21,6 +22,31 @@ public class Main {
                     "Press 4 to delete a contact\n" +
                     "Press 5 to exit program");
             int number = scanner.nextInt();
-
+            switch (number) {
+                case 1:
+                    System.out.println("You have chosen to add a new contact: \n" +
+                            "Please enter the name of the person");
+                    System.out.println("First name");
+                    FirstName = scanner.nextLine();
+                    System.out.println("Last Name");
+                    LastName = scanner.nextLine();
+                    contactList.setName(FirstName, LastName);
+                    contactList.setFirstNameInList(FirstName);
+                    System.out.println("Contact Number:");
+                    contactNumber[0] = scanner.nextLine();
+                    for (int i = 1; i <= 2; i++) {
+                        System.out.println("Would you like to add new Number?");
+                        char response = scanner.next().charAt(0);
+                        if (response == 'y') {
+                            contactNumber[i] = scanner.next();
+                        } else {
+                            break;
+                        }
+                    }
+                    System.out.println("Enter the Email:");
+                    email = scanner.next();
+                    Person person = new Person(FirstName, LastName, contactNumber, email);
+                    contactList.add(person);
+                    break;
         }
     }
